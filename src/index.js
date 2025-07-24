5aa3484463a08a6fa477a54b7ad3827ebcc211fe
svv98 <76674639+svv98@users.noreply.github.com>
2025-07-23 21:09:06 -0700

    finished all modals HTML and CSS!!

--- src/index.js
+++ src/index.js
@@ -54,3 +54,120 @@ import "./style.css";
 //     event.target.appendChild(dragged);
 //   }
 // });
+
+const addTaskVariables = (function(){
+    
+    const addTaskModal = document.querySelector('#addTaskModal');
+    const addTaskCANCEL = document.querySelector('#addTaskModal button[type="reset"]');
+    const addTaskSUBMIT = document.querySelector('#addTaskModal button[type="submit"]');
+
+    return {addTaskModal, addTaskCANCEL, addTaskSUBMIT}
+})();
+
+const addProjectVariables = (function(){
+    
+    const addProjectModal = document.querySelector('#addProjectModal');
+    const addProjectCANCEL = document.querySelector('#addProjectModal button[type="reset"]');
+    const addProjectSUBMIT = document.querySelector('#addProjectModal button[type="submit"]');
+
+    return {addProjectModal, addProjectCANCEL, addProjectSUBMIT}
+})();
+
+const infoTaskVariables = (function(){
+    
+    const infoTaskModal = document.querySelector('#infoTaskModal');
+    const infoTaskCANCEL = document.querySelector('#infoTaskModal button[type="reset"]');
+    const infoTaskSUBMIT = document.querySelector('#infoTaskModal button[type="submit"]');
+
+    return {infoTaskModal, infoTaskCANCEL, infoTaskSUBMIT}
+})();
+
+const infoProjectVariables = (function(){
+
+    const infoProjectModal = document.querySelector('#infoProjectModal');
+    const infoProjectCANCEL = document.querySelector('#infoProjectModal button[type="reset"]');
+    const infoProjectSUBMIT = document.querySelector('#infoProjectModal button[type="submit"]');
+
+    return {infoProjectModal, infoProjectCANCEL, infoProjectSUBMIT}
+})();
+
+
+/* const mainPageVariables = (function(){
+    
+    const newTask = document.querySelector('.newTask');
+    const allTasks = document.querySelector('main');
+
+    return {newTask, allTasks}
+})();
+
+mainPageVariables.newTask.addEventListener('click', ()=>{
+    addTaskVariables.addTaskModal.showModal();
+});
+addTaskVariables.addTaskCANCEL.addEventListener('click', ()=>{
+    addTaskVariables.addTaskModal.close();
+});
+
+mainPageVariables.allTasks.addEventListener('click', (event)=>{
+    if(event.target.closest("article")){
+        infoTaskVariables.infoTaskModal.showModal();
+    }
+    infoTaskVariables.infoTaskModal.addEventListener('click', (event)=>{
+        if(event.target.closest(".infoContent")){}
+        else infoTaskVariables.infoTaskModal.close();
+    });
+}); */
+
+/* const projectsPageVariables = (function(){
+    
+    const newProject = document.querySelector('.newProject');
+    const allProjects = document.querySelector('.allProjects');
+
+    return {newProject, allProjects}
+})();
+
+projectsPageVariables.newProject.addEventListener('click', ()=>{
+    addProjectVariables.addProjectModal.showModal();
+});
+addProjectVariables.addProjectCANCEL.addEventListener('click', ()=>{
+    addProjectVariables.addProjectModal.close();
+});
+
+projectsPageVariables.allProjects.addEventListener('click', (event)=>{
+        if(event.target.closest(".projectCard")){
+            console.log('hola');
+        }
+}); */
+
+const selectedProjectPageVariables = (function(){
+    
+    const newProjectTask = document.querySelector('.newProjectTask');
+    const projectMain = document.querySelector('#selectedProjectPage>main');
+    const projectTitle = document.querySelector('.headerTitle');
+
+    return {newProjectTask, projectMain, projectTitle}
+})();
+
+selectedProjectPageVariables.newProjectTask.addEventListener('click', ()=>{
+    addTaskVariables.addTaskModal.showModal();
+});
+addTaskVariables.addTaskCANCEL.addEventListener('click', ()=>{
+    addTaskVariables.addTaskModal.close();
+});
+
+selectedProjectPageVariables.projectMain.addEventListener('click', (event)=>{
+    if(event.target.closest("article")){
+        infoTaskVariables.infoTaskModal.showModal();
+    }
+    infoTaskVariables.infoTaskModal.addEventListener('click', (event)=>{
+        if(event.target.closest(".infoContent")){}
+        else infoTaskVariables.infoTaskModal.close();
+    });
+});
+
+selectedProjectPageVariables.projectTitle.addEventListener('click', (event)=>{
+    infoProjectVariables.infoProjectModal.showModal();
+    infoProjectVariables.infoProjectModal.addEventListener('click', (event)=>{
+        if(event.target.closest(".infoContent")){}
+        else infoProjectVariables.infoProjectModal.close();
+    });
+});
