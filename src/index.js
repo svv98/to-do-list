5be6052dca87aee8788361a2cb13050ab4451931
svv98 <76674639+svv98@users.noreply.github.com>
2025-07-25 21:07:26 -0700

    continued with DOM elements

--- src/index.js
+++ src/index.js
@@ -8,23 +8,62 @@ const pages = (function(){
 
     return {mainPage, projectsPage, selectedProjectPage}
 })();
-const addTaskVariables = (function(){
+const addTaskEvents = (function(){
     
     const addTaskModal = document.querySelector('#addTaskModal');
+    const addTaskForm = document.querySelector('#addTaskForm');
     const addTaskCANCEL = document.querySelector('#addTaskModal button[type="reset"]');
-    const addTaskSUBMIT = document.querySelector('#addTaskModal button[type="submit"]');
 
-    return {addTaskModal, addTaskCANCEL, addTaskSUBMIT}
+    addTaskForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        const formData = new FormData(this);
+
+        const title = formData.get('taskTitle');
+        const project = formData.get('taskProject');
+        const description = formData.get('taskDescription');
+        const date = formData.get('taskDueDate');
+        const priority = formData.get('taskPriotity');
+
+        let newtask = new NewTask(title, project, description, date, priority);
+        newtask.add(Object.assign({}, newtask));
+        addTaskModal.close();
+    });
+
+    addTaskCANCEL.addEventListener('click', ()=>{
+        addTaskModal.close();
+    });
+
+    return {addTaskModal}
 })();
-const addProjectVariables = (function(){
+const addProjectEvents = (function(){
     
     const addProjectModal = document.querySelector('#addProjectModal');
+    const addProjectForm = document.querySelector('#addProjectForm');
     const addProjectCANCEL = document.querySelector('#addProjectModal button[type="reset"]');
-    const addProjectSUBMIT = document.querySelector('#addProjectModal button[type="submit"]');
 
-    return {addProjectModal, addProjectCANCEL, addProjectSUBMIT}
+    addProjectForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        const formData = new FormData(this);
+
+        const title = formData.get('projectTitle');
+        const description = formData.get('projectDescription');
+        const date = formData.get('projectDueDate');
+        const priority = formData.get('projectPriotity');
+
+        let newproject = new NewProject(title, description, date, priority);
+        newproject.add(Object.assign({}, newproject));
+        addProjectModal.close();
+        addProjectForm.reset();
+    });
+
+    addProjectCANCEL.addEventListener('click', ()=>{
+        addProjectModal.close();
+        addProjectForm.reset();
+    });
+
+    return {addProjectModal}
 })();
-const infoTaskVariables = (function(){
+const infoTaskEvents = (function(){
     
     const infoTaskModal = document.querySelector('#infoTaskModal');
     const infoTaskCANCEL = document.querySelector('#infoTaskModal button[type="reset"]');
@@ -32,7 +71,7 @@ const infoTaskVariables = (function(){
 
     return {infoTaskModal, infoTaskCANCEL, infoTaskSUBMIT}
 })();
-const infoProjectVariables = (function(){
+const infoProjectEvents = (function(){
 
     const infoProjectModal = document.querySelector('#infoProjectModal');
     const infoProjectCANCEL = document.querySelector('#infoProjectModal button[type="reset"]');
@@ -44,6 +83,7 @@ const infoProjectVariables = (function(){
 let tasks = [ ];
 let projects = [ ];
 let currentPage = 'home';
+let sidebarProjectCounter = 0;
 
 class NewTask{
     constructor(title, project, description, date, priority){
@@ -54,7 +94,7 @@ class NewTask{
         this.date = date;
         this.priority = priority;
         this.status = "todo";
-        this.added = false;
+        this.added = false
     }
     add(task){
         tasks.push(task);
@@ -73,28 +113,22 @@ class NewProject{
             toDo: [ ],
             doing: [ ],
             done: [ ],
+            get total() {
+                return this.toDo.length + this.doing.length + this.done.length;
+            }
         };
     }
     add(project){
         projects.push(project);
-        // addToDOM().taskDOM();
+        addToDOM().projectDOM();
     }
 }
-
-let task1 = new NewTask('task1', undefined, 'task1Description', '2025-08-10', 'low');
-task1.add(Object.assign({}, task1));
-
-let task2 = new NewTask('task2', undefined, 'task2Description', '2025-08-10', 'low');
-task2.add(Object.assign({}, task2));
-
-// let project1 = new NewProject('project1', 'project1Description', '2025-08-10', 'imp');
-// project1.add(Object.assign({}, project1));
-// console.log(project1);
 
 const sidebar = (function(){
     mainPageEvents();
     const homeButton = document.querySelector(".home");
-    const projectsButton = document.querySelector(".sbProjects");
+    const projectsButton = document.querySelector(".sbProjectsButton");
+    const sdProjectList = document.querySelector(".sdProjectList");
     const addProjectButton = document.querySelector(".sbAddProject");
 
     homeButton.addEventListener('click',()=>{
@@ -103,9 +137,6 @@ const sidebar = (function(){
             pages.selectedProjectPage.classList.add('hide');
             pages.mainPage.classList.remove('hide');
 
-            for(let task of tasks){
-                task.added=false;
-            }
             addToDOM().taskDOM();
             currentPage='home';
         }
@@ -117,6 +148,19 @@ const sidebar = (function(){
         projectsPageEvents();
         currentPage='projects';
     });
+    addProjectButton.addEventListener('click',()=>{
+        addProjectEvents.addProjectModal.showModal();
+    });
+    sdProjectList.addEventListener('click', (event)=>{
+            if(event.target.closest(".projectListItem")){
+                let selectedProject = event.target.closest(".projectListItem");
+                selectedProjectPage(selectedProject.id);
+                console.log(selectedProject.id);
+                addToDOM().selectedProjectDOM(selectedProject.id);
+            }
+    });
+
+    return {sdProjectList}
 })();
 function mainPageEvents(){
     
@@ -124,18 +168,15 @@ function mainPageEvents(){
     const allTasks = document.querySelector('main');
 
     newTask.addEventListener('click', ()=>{
-        addTaskVariables.addTaskModal.showModal();
-    });
-    addTaskVariables.addTaskCANCEL.addEventListener('click', ()=>{
-        addTaskVariables.addTaskModal.close();
+        addTaskEvents.addTaskModal.showModal();
     });
     allTasks.addEventListener('click', (event)=>{
         if(event.target.closest("article")){
-            infoTaskVariables.infoTaskModal.showModal();
+            infoTaskEvents.infoTaskModal.showModal();
         }
-        infoTaskVariables.infoTaskModal.addEventListener('click', (event)=>{
+        infoTaskEvents.infoTaskModal.addEventListener('click', (event)=>{
             if(event.target.closest(".infoContent")){}
-            else infoTaskVariables.infoTaskModal.close();
+            else infoTaskEvents.infoTaskModal.close();
         });
     });
     
@@ -146,14 +187,13 @@ function projectsPageEvents(){
     const allProjects = document.querySelector('.allProjects');
 
     newProject.addEventListener('click', ()=>{
-        addProjectVariables.addProjectModal.showModal();
-    });
-    addProjectVariables.addProjectCANCEL.addEventListener('click', ()=>{
-        addProjectVariables.addProjectModal.close();
+        addProjectEvents.addProjectModal.showModal();
     });
     allProjects.addEventListener('click', (event)=>{
             if(event.target.closest(".projectCard")){
-                console.log('hola');
+                let selectedProject = event.target.closest(".projectCard");
+                selectedProjectPage(selectedProject.id);
+                addToDOM().selectedProjectDOM();
             }
     });
 };
@@ -162,38 +202,49 @@ function selectedProjectPageEvents(){
     const newProjectTask = document.querySelector('.newProjectTask');
     const projectMain = document.querySelector('#selectedProjectPage>main');
     const projectTitle = document.querySelector('.headerTitle');
+    const projectTitleH1 = document.querySelector('.headerTitle>h1');
+    const projectProgressBar = document.querySelector('.projectProgressBar');
+    const projectDueDate = document.querySelector('.projectDueDate>.date');
 
     newProjectTask.addEventListener('click', ()=>{
-        addTaskVariables.addTaskModal.showModal();
-    });
-    addTaskVariables.addTaskCANCEL.addEventListener('click', ()=>{
-        addTaskVariables.addTaskModal.close();
+        addTaskEvents.addTaskModal.showModal();
     });
     projectMain.addEventListener('click', (event)=>{
         if(event.target.closest("article")){
-            infoTaskVariables.infoTaskModal.showModal();
+            infoTaskEvents.infoTaskModal.showModal();
         }
-        infoTaskVariables.infoTaskModal.addEventListener('click', (event)=>{
+        infoTaskEvents.infoTaskModal.addEventListener('click', (event)=>{
             if(event.target.closest(".infoContent")){}
-            else infoTaskVariables.infoTaskModal.close();
+            else infoTaskEvents.infoTaskModal.close();
         });
     });
     projectTitle.addEventListener('click', (event)=>{
-        infoProjectVariables.infoProjectModal.showModal();
-        infoProjectVariables.infoProjectModal.addEventListener('click', (event)=>{
+        infoProjectEvents.infoProjectModal.showModal();
+        infoProjectEvents.infoProjectModal.addEventListener('click', (event)=>{
             if(event.target.closest(".infoContent")){}
-            else infoProjectVariables.infoProjectModal.close();
+            else infoProjectEvents.infoProjectModal.close();
         });
     });
+
+    return {projectTitleH1,projectProgressBar,projectDueDate}
 };
+function selectedProjectPage(id){
+    pages.mainPage.classList.add('hide');
+    pages.projectsPage.classList.add('hide');
+    pages.selectedProjectPage.classList.remove('hide');
+    selectedProjectPageEvents()
+    currentPage=id;
+}
 
 function addToDOM(){
     let taskDOM = ()=>{
-        console.log('taskDOM');
+        // console.log('taskDOM');
         for(let task of tasks){
             if(task.added === false){
                 console.log(task);
                 task.added=true;
+
+                // main Page
                 const card = document.createElement("article");
                 card.id = task.id;
                 card.classList.add("card");
@@ -210,12 +261,87 @@ function addToDOM(){
 
                 let list = styleFunctions().column(task.status);
                 list.appendChild(card);
+
             }
         }
     }
 
+    let projectDOM = ()=>{
+        for(let project of projects){
+            if(project.added === false){
+                console.log(project);
+                project.added=true;
 
-    return {taskDOM}
+                // projects page
+                const projectCard = document.createElement("article");
+                projectCard.classList.add("projectCard");
+                projectCard.id = project.id;
+
+                const projectPriority = document.createElement("div");
+                projectPriority.classList.add("projectPriority");
+                projectPriority.style = `background-color: ${styleFunctions().priorityColor(project.priority)};`;
+                projectCard.appendChild(projectPriority);
+
+                const projectTitle = document.createElement("p");
+                projectTitle.classList.add("projectTitle");
+                projectTitle.textContent = project.title;
+                projectCard.appendChild(projectTitle);
+
+                const progressDIV = document.createElement("p");
+                progressDIV.classList.add("progressDIV");
+                const progressBar = document.createElement("progress");
+                progressBar.classList.add("progressBar");
+                progressBar.max=100;
+                progressBar.value = (project.tasks.total == 0)? 0 : parseInt(project.tasks.done.length * 100 / project.tasks.total);
+                project.progress= progressBar.value;
+
+                progressDIV.appendChild(progressBar);
+                projectCard.appendChild(progressDIV);
+
+                let list = document.querySelector('.allProjects');
+                list.appendChild(projectCard);
+
+                projectSidebarDOM();
+            }
+        }
+        function  projectSidebarDOM(){
+            console.log(projects);
+           if(sidebarProjectCounter<5){
+                const projectListItem = document.createElement('li');
+                projectListItem.id = projects[projects.length-1].id;
+                projectListItem.classList.add('projectListItem');
+                projectListItem.textContent = projects[projects.length-1].title;
+
+                sidebar.sdProjectList.appendChild(projectListItem);
+                sidebarProjectCounter++;
+           }
+        }
+    }
+
+    let selectedProjectDOM = (id)=>{
+        let title, progress, date;
+        for(let project of projects){
+            if(project.id == id){
+                title = project.title;
+                progress = project.progress;
+                date = project.date;
+            }
+        }
+        selectedProjectPageEvents().projectTitleH1.textContent = title;
+        selectedProjectPageEvents().projectProgressBar.value = progress;
+        selectedProjectPageEvents().projectDueDate.textContent = date;
+        if(!(document.querySelector('#selectedProjectPage .todo.list').hasChildNodes())){
+            console.log('noChild');
+        }
+        if(!(document.querySelector('#selectedProjectPage .doing.list').hasChildNodes())){
+            console.log('noChild');
+        }
+        if(!(document.querySelector('#selectedProjectPage .done.list').hasChildNodes())){
+            console.log('noChild');
+        }
+    }
+
+    return {taskDOM, projectDOM, selectedProjectDOM}
 }
 
 function styleFunctions(){
@@ -223,13 +349,13 @@ function styleFunctions(){
         let list;
         switch(status){
             case 'todo':
-                list = document.querySelector('.todo.list');
+                list = document.querySelector('#mainPage .todo.list');
                 break;
             case 'doing':
-                list = document.querySelector('.doing.list');
+                list = document.querySelector('#mainPage .doing.list');
                 break;
             case 'done':
-                list = document.querySelector('.done.list');
+                list = document.querySelector('#mainPage .done.list');
                 break;
         }
         return list
@@ -257,4 +383,39 @@ function styleFunctions(){
     };
     return{priorityColor, column}
 }
-selectedProjectPageEvents();
+
+let task1 = new NewTask('task1', undefined, 'task1Description', '2025-08-10', 'low');
+task1.add(Object.assign({}, task1));
+
+let task2 = new NewTask('task2', undefined, 'task2Description', '2025-08-11', 'mod');
+task2.status='doing';
+task2.add(Object.assign({}, task2));
+
+let task3 = new NewTask('task3', undefined, 'task3Description', '2025-05-04', 'imp');
+task3.status='done';
+task3.add(Object.assign({}, task3));
+
+
+let project1 = new NewProject('project1', 'project1Description', '2025-08-10', 'imp');
+project1.tasks.toDo.push(1);
+project1.tasks.done.push(1);
+project1.tasks.done.push(1);
+project1.add(Object.assign({}, project1));
+
+let project2 = new NewProject('project2', 'project2Description', '2025-08-11', 'mod');
+project2.tasks.toDo.push(1);
+project2.tasks.toDo.push(1);
+project2.tasks.done.push(1);
+project2.add(Object.assign({}, project2));
+
+let project3 = new NewProject('project3', 'project3Description', '2025-08-15', 'vim');
+project3.add(Object.assign({}, project3));
+
+let project4 = new NewProject('project4', 'project4Description', '2025-08-10', 'imp');
+project4.add(Object.assign({}, project4));
+
+let project5 = new NewProject('project5', 'project5Description', '2025-08-10', 'mod');
+project5.add(Object.assign({}, project5));
+
+let project6 = new NewProject('project6', 'project6Description', '2025-08-10', 'vim');
+project6.add(Object.assign({}, project6));
